d56022609ee0c0cb68e165e31c47c3561e21c573
davin octavian <davinoctavian@gmail.com>
2023-03-08 12:00:29 +0700

    add insert username page and dashboard

--- src/app/context/username.js
+++ src/app/context/username.js
@@ -1,11 +1,18 @@
 'use client'
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const UsernameContext = createContext({})
 
 export const UsernameContextProvider = ({ children }) => {
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        const user = localStorage.getItem("username")
+        if (user) {
+            setUsername(user)
+        }
+    }, [])
 
     return (
         <UsernameContext.Provider value={{ username, setUsername }}>
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,60 @@
+'use client'
+import { Inter } from 'next/font/google'
+import styles from './dashboard.module.css'
+import { useUsernameContext } from '../context/username'
+import { use, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Dashboard() {
+  const router = useRouter()
+  const { username, setUsername} = useUsernameContext()
+  const [ data, setData ] = useState([])
+
+  useEffect(() =>{
+    if (username == '') return
+    const callApi = async() => {
+        try {
+            const res = await fetch("https://api.github.com/users/" +username+"/repos", {
+                headers: {
+                "Content-Type": "application/json",
+                },
+                method: "GET",
+            })
+            const result = await res.json()
+            setData(result)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    callApi()
+  }, [username])
+
+  return (
+    <div className={styles.agFormatContainer}>
+        <h2 className={styles.titleDocument}>{username} <span onClick={() => router.push('/')}><sup>X</sup></span></h2>
+        <div className={styles.agCoursesBox}>
+            {data && Array.isArray(data) && data.map((x) => 
+                <div className={styles.agCoursesItem} key={x.id}>
+                    <a href={x.html_url} className={styles.agCoursesItemLink}>
+                        <div className={styles.agCoursesItemBg}></div>
+                
+                        <div className={styles.agCoursesItemTitle}>
+                            {x.name.length > 19 ? x.name.substring(0, 18)+'...' : x.name}
+                        </div>
+                
+                        <div className={styles.agCoursesItemDateBox}>
+                            Start:
+                            <span className={styles.agCoursesItemDate}>
+                            {String(x.created_at).split('T')[0]}
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            )}
+            
+        </div>
+    </div>
+  )
+}
